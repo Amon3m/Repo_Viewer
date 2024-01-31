cad02355744b85f5f8c9cfa946385517a3972112
Ahmed Abd El-Monem <112562093+Amon3m@users.noreply.github.com>
2024-01-31 18:46:03 +0200

    establish libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    id("kotlin-kapt")
+    id ("androidx.navigation.safeargs.kotlin")
+    id ("com.google.dagger.hilt.android")
 }
 
 android {
@@ -30,6 +34,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding=true
+    }
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -44,4 +51,47 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Navigation component
+    val nav_version = "2.7.5"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    //data store
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    //Coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.0")
+
+    //ViewModel & livedata
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.2")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.5.2")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    //Dagger - Hilt
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+
+    //Logging Interceptor
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    //Room
+    implementation ("androidx.room:room-ktx:2.6.1")
+    implementation ("androidx.room:room-runtime:2.6.1")
+    kapt ("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-paging:2.6.1")
+
+    //paging 3
+
+    implementation("androidx.paging:paging-runtime:3.2.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,16 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id ("com.google.dagger.hilt.android") version "2.48.1" apply false
+
+}
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.7.5"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+
+    }
 }
