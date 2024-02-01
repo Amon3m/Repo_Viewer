b1425bd6f8cb6245fb3a1c41ebb71f1a5563cbde
Ahmed Abd El-Monem <112562093+Amon3m@users.noreply.github.com>
2024-02-01 13:06:10 +0200

    make api service Create models

---
+++ app/src/main/java/com/example/repoviewer/network/ApiService.kt
@@ -0,0 +1,26 @@
+package com.example.repoviewer.network
+
+import com.example.repoviewer.model.DetailsResponse
+import com.example.repoviewer.model.IssuesResponse
+import com.example.repoviewer.model.RepositoriesResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+
+    @GET("repositories")
+    suspend fun getRepositories(): RepositoriesResponse
+
+    @GET("/repos/{owner}/{repo}")
+    suspend fun getRepoDetails(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ): DetailsResponse
+
+    @GET("repos/{owner}/{repo}/issues")
+    suspend fun getRepoIssues(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ): IssuesResponse
+}
